e39e6d788de7d8bf1e046302fc28997002bc252a
Alejandro Henriquez <alejandroicod@gmail.com>
2021-12-21 14:27:34 +0000

    Last column margin

--- column-grid/script.js
+++ column-grid/script.js
@@ -1,7 +1,14 @@
+
+function updateSize() {
+  $(".main .column").each(function () {
+    $(this).text(Math.round($(this).width() * 10) / 10 + "px");
+    $('.main .column').removeClass("last");
+    $('.main .column:visible:last').addClass("last");
+  });
+}
 $(document).ready(function () {
+  updateSize();
   $(window).resize(function () {
-    $(".main .column").each(function () {
-      $(this).text(Math.round($(this).width() * 10) / 10);
-    });
+    updateSize();
   });
 });
